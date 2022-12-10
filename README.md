bcc63e29344c45494d4e8cfb0f901a9f8b57e249
Cédric Donner <cedonner@gmail.com>
2022-12-10 16:33:13 +0000

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# oci-linux
+# Dépôt de base pour apprendre à utiliser Linux
+
+Ce dépôt permet fait partie du cours d'option complémentaire et sert à apprendre Linux en ligne de commande grâce à l'environnement https://gitpod.io/.
+
---
+++ compilation/sum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int result = 0;
+  int n;
+
+  if (argc == 2) {
+    n = atoi(argv[1]);
+  } else if (argc == 1) {
+    n = 100;
+  } else {
+    printf("Usage: %s <N>", argv[0]);
+  }
+
+  for (int i = 1; i <= n; i += 1) {
+    result += i;
+  }
+
+  printf("The of integers up to %d is %d\n", n, result);
+  return 0;
+}
